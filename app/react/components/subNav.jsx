528068a96738d2065e1ffa569f51f1de3e7a2f80
Khaled Jouda <khaled.jouda@ymail.com>
2014-07-17 01:21:24 +0200

    Update subNav.jsx

--- app/react/components/subNav.jsx
+++ app/react/components/subNav.jsx
@@ -3,19 +3,29 @@ var React = require('react');
 var _ = require('underscore');
 
 module.exports = React.createClass({
-	render: function(){
-		var elements = [];
-		_.each(this.props.links, function(item, key){
-			elements.push(
-				<li key={key}><a href={item.url}>{item.label}</a></li>
-			);
-		})
-		return(
-		<div className="sub-nav">
-      <ul>
-      {elements}
-      </ul>
-    </div>
-   );
-	}
+  
+  propTypes: {
+    links: React.PropTypes.array.isRequired
+  },
+  
+  render: function(){
+  
+    var elements = [];
+    
+    _.each(this.props.links, function(item, key){
+    	elements.push(
+          <li key={key}><a href={item.url}>{item.label}</a></li>
+	);
+    });
+    
+    return(
+      <div className="sub-nav">
+        <ul>
+          {elements}
+        </ul>
+      </div>
+    );
+    
+  }
+  
 });
