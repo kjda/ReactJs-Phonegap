99ae50882f5a5dd7b8b030ad40920c9a60dbf26e
kjda <khaled.jouda@ymail.com>
2014-08-07 23:22:16 +0200

    test .hash

--- app/src/pages/logout.jsx
+++ app/src/pages/logout.jsx
@@ -24,7 +24,7 @@ module.exports = React.createClass({
 	onUserChange: function(){
 		if( !UserStore.isAuth() ){
 			setTimeout(function(){
-			window.location.href = '#';
+				window.location.hash = '#';
 			}, 0);
 		}
 		else{
